974a3c8ae6690c8a9e5af7dbcb70b6a9a270eed1
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-09 10:20:06 +0100

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,11 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://andy-capstone-week-05.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://andy-capstone-week-05.herokuapp.com', 'https://andy-capstone-week-05.herokuapp.com' /http:\/\/andy-capstone-week-05.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [
+    'http://andy-capstone-week-05.herokuapp.com',
+    'https://andy-capstone-week-05.herokuapp.com',
+    /http:\/\/andy-capstone-week-05.herokuapp.*/
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
