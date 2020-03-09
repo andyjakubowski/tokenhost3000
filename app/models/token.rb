8b3e38a6b0d0b01e133af3336f63f4313b8e859a
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-09 18:38:15 +0100

    Fix JSON response

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,6 +2,6 @@ class Token < ApplicationRecord
   belongs_to :list
   belongs_to :category, optional: true
 
-  # after_commit { self.list.update_stylesheet }
-  # after_rollback { self.list.update_stylesheet }
+  after_commit { self.list.update_stylesheet }
+  after_rollback { self.list.update_stylesheet }
 end
---
+++ app/views/spaces/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @space.lists, partial: "lists/list", as: :list
