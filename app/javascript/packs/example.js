a91d86b9b618f2a0a9a71ad551f7814af2af892d
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-11 10:38:25 +0100

    Set up JS list switcher for example page

---
+++ app/javascript/packs/example.js
@@ -0,0 +1,94 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+const App = (function App() {
+  const SPACE_NAME = document.querySelector('meta[name="space-slug"]').content;
+  const root = document.documentElement;
+  const storage = window.localStorage;
+  let ul;
+
+  const activeListId = function activeListId() {
+    return Number(document.querySelector('meta[name="list-id"]').content) || null;
+  };
+
+  const getLists = function getLists() {
+    return fetch(`http://localhost:3027/spaces/${SPACE_NAME}.json`)
+      .then((response) => response.json());
+  };
+
+  const getTokens = function getTokens(listId) {
+    return fetch(`http://localhost:3027/lists/${listId}/tokens.json`)
+      .then((response) => response.json());
+  };
+
+  const selectList = function selectList(li) {
+    const activeLi = document.querySelector('li[data-active]');
+    activeLi.classList.remove('token-list-switcher__list-item_active');
+    activeLi.removeAttribute('data-active');
+
+    li.classList.add('token-list-switcher__list-item_active');
+    li.setAttribute('data-active', '');
+    storage.setItem(`activeListId#${SPACE_NAME}`, li.dataset.id);
+  };
+
+  const listItemClicked = function listItemClicked(e) {
+    const li = e.target;
+    selectList(li);
+
+    getTokens(li.dataset.id)
+      .then((tokens) => updateStyles(tokens));
+  };
+
+  const updateStyles = function updateStyles(tokens) {
+
+    root.style = '';
+
+    tokens.forEach(({ name, value }, index) => {
+      root.style.setProperty(`--remote-${name}`, value);
+    });
+  };
+
+  const updateListSwitcher = function updateListSwitcher(lists) {
+    ul.innerHTML = '';
+
+    lists.forEach((list) => {
+      const li = document.createElement('li');
+
+      li.innerText = list.name;
+      li.classList.add('token-list-switcher__list-item');
+      li.setAttribute('data-id', list.id);
+      if (list.id === activeListId()) {
+        li.setAttribute('data-active', '');
+        li.classList.add('token-list-switcher__list-item_active');
+        getTokens(list.id)
+          .then((tokens) => updateStyles(tokens));
+      }
+
+      li.addEventListener('click', (e) => listItemClicked(e));
+      ul.append(li);
+    });
+  };
+
+  return {
+    init () {
+      ul = document.querySelector('.token-list-switcher__lists');
+
+      getLists()
+        .then((lists) => {
+          if (!activeListId()) {
+            storage.setItem(`activeListId#${SPACE_NAME}`, lists[0].id)
+          }
+
+          updateListSwitcher(lists);
+        });
+    },
+  };
+}());
+
+if (document.readyState !== 'loading') {
+  App.init();
+} else {
+  document.addEventListener('DOMContentLoaded', App.init)
+}
