53886441ac40eff0c945d0cafe5e3c6b1aa7b92b
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-16 09:51:13 +0100

    Add FontSize token type visual

--- app/helpers/tokens_helper.rb
+++ app/helpers/tokens_helper.rb
@@ -3,12 +3,27 @@ module TokensHelper
     Token.types
   end
 
+  def classes_for_visual(token)
+    classString = "token__content-visual token__content-visual_"
+
+    case token.type
+    when 'Color'
+      modifier = 'color'
+    when 'FontSize'
+      modifier = 'font-size'
+    else
+      modifier = ''
+    end
+
+    classString + modifier
+  end
+
   def styles_for_visual(token)
     case token.type
     when 'Color'
       "background-color: #{token.value};"
     when 'FontSize'
-      "background-color: yellowgreen;"
+      "font-size: #{token.value}px;"
     else
       ''
     end
