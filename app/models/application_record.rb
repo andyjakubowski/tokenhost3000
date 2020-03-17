9ae1622353e829d978227d49fe9dd05e94bd44a6
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-17 19:53:40 +0100

    Generate long random ID for each created model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
+  before_create :randomize_id
+
   self.abstract_class = true
+
+  private
+    def randomize_id
+      loop do
+        self.id = SecureRandom.random_number(10_000_000)
+        break unless self.class.where(id: self.id).exists?
+      end
+    end
 end
