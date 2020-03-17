b3550dbed791cd362d5065f82f01e24dfe885be7
Andy Jakubowski <hello@andyjakubowski.com>
2020-03-17 15:33:47 +0100

    Add description column to lists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_143501) do
+ActiveRecord::Schema.define(version: 2020_03_17_143153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_143501) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "space_id", default: 1, null: false
+    t.text "description"
     t.index ["space_id"], name: "index_lists_on_space_id"
   end
 
